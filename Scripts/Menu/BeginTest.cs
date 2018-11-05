faec4390d5ae41f36cfcc8b24039848a9e880831
Arthur <ahmed.sdlv@gmail.com>
2018-11-05 10:45:53 +0100

    updt

---
+++ Scripts/Menu/BeginTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AccuracyTestManager : MonoBehaviour
+{
+    private string AccuracyTest = "CircleTest";
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Y))
+            StartCoroutine(LoadCurrentScene());
+		if(Input.GetKeyUp(KeyCode.N))
+			gameObject.SetActive(false);
+    }
+
+    IEnumerator LoadCurrentScene()
+    {
+        AsyncOperation asyncScene = SceneManager.LoadSceneAsync(AccuracyTest
+            , LoadSceneMode.Additive);
+
+        while (!asyncScene.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Scripts/Menu/LeaveTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LeaveTest : MonoBehaviour
+{
+
+    private string AccuracyTest = "CircleTest";
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+            SceneManager.UnloadSceneAsync(AccuracyTest);
+    }
+}
